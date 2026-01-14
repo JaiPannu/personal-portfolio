df1fcb35341f9a80ddd12d1f19dd102683ab119f
Jai Singh Pannu <dev.jaipannu@gmail.com>
2026-01-14 18:34:31 -0500

    why does in keep changing bro

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
           </div>
           <div className="contact-item">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="contact-icon"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
-            <a href="https://www.linkedin.com/in/jai-pannu" target="_blank" rel="noopener noreferrer">
+            <a href="www.linkedin.com/in/jai-pannu" target="_blank" rel="noopener noreferrer">
               linkedin.com/in/jai-pannu
             </a>
           </div>
