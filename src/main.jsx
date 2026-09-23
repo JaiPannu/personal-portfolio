5e81ed202ce9fb49000a65ae2d3b2154851a729a
Jai Singh Pannu <dev.jaipannu@gmail.com>
2026-09-23 15:31:58 -0400

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 
@@ -9,5 +10,6 @@ document.documentElement.dataset.theme = savedTheme === 'paper' ? 'paper' : 'sol
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
